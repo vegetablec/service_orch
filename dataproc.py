ead7a407b9ddfdab058334dc04c05a29a0d456cb
vegetablec <1726788550@qq.com>
2023-03-24 00:38:23 +0800

    dataproc

---
+++ dataproc.py
@@ -0,0 +1,12 @@
+import pymysql
+import random
+
+conn = pymysql.connect(host='42.193.113.93', user='root', password='291482jX', db='qws')
+cursor = conn.cursor()
+sql = 'select id,RT,Avai,Thr,Succ,Name from qws2;'
+cursor.execute(sql)
+data = []
+data = cursor.fetchall()
+print(data)
+random.shuffle(data)
+print(data)
