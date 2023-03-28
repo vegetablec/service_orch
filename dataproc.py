291edb341899cfcc476953e78c03d8d7c7da586c
vegetablec <65810346+vegetablec@users.noreply.github.com>
2023-03-28 23:26:45 +0800

    Update dataproc.py

--- dataproc.py
+++ dataproc.py
@@ -5,9 +5,8 @@ conn = pymysql.connect(host='42.193.113.93', user='root', password='291482jX', d
 cursor = conn.cursor()
 sql = 'select id,RT,Avai,Thr,Succ,Name from qws2;'
 cursor.execute(sql)
-data = []
 data = cursor.fetchall()
-print(data)
-random.shuffle(data)
-print(data)
+nodedata = []
+for i in range(0,10):
+  nodedata.append(random.sample(data, random.randint(1, 1000)))
 
